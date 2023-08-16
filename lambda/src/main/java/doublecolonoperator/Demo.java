77262669a772bf7fe3fc885e9a72f4a5683505cc
yoonisabdurahman <yoonis215@gmail.com>
2023-08-15 23:22:19 -0700

    Double Colon Operator

---
+++ lambda/src/main/java/doublecolonoperator/Demo.java
@@ -0,0 +1,35 @@
+package doublecolonoperator;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+public class Demo {
+    public static void main(String[] args) {
+        //referance tp static  method
+        Calculate s1 =(x,y) ->Calculator.findSum(x,y);
+        s1.calculate(10,20);
+        // line 6 and line 9 referance the same method
+        Calculate s2 = Calculator ::findSum;
+        s2.calculate(100,35);
+
+        //Referance to instance method
+        Calculate m1 = (x,y) -> new Calculator().findMultiply(x,y);
+        m1.calculate(5,6);
+
+        Calculator obj = new Calculator();
+        Calculate m2 = obj ::findMultiply;
+        m2.calculate(8,9);
+
+        Calculate m3 = new Calculator()::findMultiply;
+        m3.calculate(6,6);
+
+        BiFunction<String,Integer,String> fn = (str,i) -> str.substring(i);
+        System.out.println(fn.apply("Developer", 6));
+
+        BiFunction<String,Integer,String> fn2 = String::substring;
+        System.out.println(fn2.apply("Developer",6));
+
+        Consumer<Integer> display = i -> System.out.println(i);
+        display.accept(20);
+    }
+}
