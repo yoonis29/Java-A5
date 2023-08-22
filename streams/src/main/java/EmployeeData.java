631214138266fae80d260b43e818cdeb7e23c36d
yoonisabdurahman <yoonis215@gmail.com>
2023-08-21 22:36:35 -0700

    created matching and reducing methods practices

---
+++ streams/src/main/java/EmployeeData.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class EmployeeData {
+    public static Stream<Employee> readAll(){
+        return Stream.of(
+                new Employee(100,"Mike","mike@cydeo.com", Arrays.asList("2029124536","20223464768")),
+                new Employee(101,"Ozzy","ozzy@cydeo.com", Arrays.asList("7039124536","70323464768")),
+                new Employee(102,"Peter","peter@cydeo.com", Arrays.asList("5169124536","51623464768"))
+        );
+    }
+}
---
+++ streams/src/main/java/MatchingAndFinding.java
@@ -0,0 +1,33 @@
+import java.util.Optional;
+
+public class MatchingAndFinding {
+    //ALL MATCH
+    public static void main(String[] args) {
+        boolean isHealthy =DishData.getAll().stream().allMatch(dish -> dish.getCalories()<1000);
+        System.out.println(isHealthy);
+
+        //AnyMatch
+        System.out.println("*******************************");
+        if (DishData.getAll().stream().anyMatch(Dish::isVegetarian))
+            System.out.println("The menu is vegetarian friendly");
+
+        //NONMATCH
+        System.out.println("***********************");
+        boolean isHealthy2 = DishData.getAll().stream().noneMatch(dish -> dish.getCalories()>=1000);
+        System.out.println(isHealthy2);
+
+        //FIND ANY
+        System.out.println("**********************");
+        Optional<Dish> dish =DishData.getAll().stream()
+                .filter(Dish::isVegetarian)
+                .findAny();
+        System.out.println(dish);
+
+        //FIND FIRST
+        System.out.println("*********************");
+        Optional<Dish> dish2 =DishData.getAll().stream()
+                .filter(Dish::isVegetarian)
+                .findFirst();
+        System.out.println(dish);
+    }
+}
