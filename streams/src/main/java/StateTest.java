d1e160c095d3b20a474335aa2e162b35452a523b
yoonisabdurahman <yoonis215@gmail.com>
2023-08-22 00:10:03 -0700

    completed Stream Task

---
+++ streams/src/main/java/StateTest.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StateTest {
+    public static void main(String[] args) {
+        State va = new State();
+        va.addCity("Mclean");
+        va.addCity("Arlington");
+        va.addCity("Fairfax");
+        va.addCity("FallsChurch");
+
+        State tx = new State();
+        tx.addCity("Dallas");
+        tx.addCity("Plano");
+        tx.addCity("Austin");
+        tx.addCity("Houston");
+
+        List<State> list = Arrays.asList(va,tx);
+//        list.stream().map(state -> state.getCity()).forEach(System.out::println);
+
+        list.stream()
+                .map(state ->state.getCity())
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+    }
+}
---
+++ streams/src/main/java/Transaction.java
@@ -0,0 +1,10 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data@AllArgsConstructor
+public class Transaction {
+    private Trader trader;
+    private int year;
+    private int value;
+
+}
